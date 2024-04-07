879cffe1d630cd1c000aba5e928f670367a34702
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2024-04-07 18:11:05 -0300

    página de login/cadastro com alteração dos campos a partir do valor do states com imagens randomicas do unsplash  completo

---
+++ src/components/ButtonSumit.jsx
@@ -0,0 +1,9 @@
+const ButtonSumit = ({ text }) => {
+  return (
+    <button className="w-full px-5 py-4 mt-[18px] rounded transition-all bg-black hover:bg-dark-blue text-white font-semibold">
+      {text}
+    </button>
+  );
+};
+
+export default ButtonSumit;
