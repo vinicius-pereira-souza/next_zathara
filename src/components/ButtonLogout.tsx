5c228167caf2a02b627b0e1670ff839fd2643b02
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2025-09-24 21:13:59 -0300

    feature: banner
    implementar a imagem de banner com a api do unsplash

--- src/components/ButtonLogout.tsx
+++ src/components/ButtonLogout.tsx
@@ -1,11 +1,11 @@
-import useAuthentication from "@/hook/useAuthentication";
+// import useAuthentication from "@/hook/useAuthentication";
 
 const ButtonLogout = ({ icon, navClosed }) => {
-  const { logoutUser } = useAuthentication();
+  // const { logoutUser } = useAuthentication();
 
   return (
     <button
-      onClick={() => logoutUser()}
+      // onClick={() => logoutUser()}
       className="relative z-40 flex items-center gap-4 py-5 min-w-full px-[30px] cursor-pointer transition-all  hover:bg-medium-gray hover:text-red-600 border-t border-solid border-gray-800"
     >
       <div className="text-[22px]">{icon}</div>
